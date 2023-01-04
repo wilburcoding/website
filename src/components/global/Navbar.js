6600cbcb80e8636202226144f4b8035c08b4eb15
Wilbur Wei <66228004+wilburcoding@users.noreply.github.com>
2023-01-04 16:09:06 -0500

    Update Navbar.js

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -41,8 +41,7 @@ export default function Navbar() {
         </div>
         <div className="flex flex-row gap-x-4 items-center justify-end w-auto flex-shrink-0">
           <ThemeSwitch />
-          {status === "authenticated" ? (
-            <>
+             <>
               <Link href="/attendance">
                 <div
                   onMouseEnter={() => setTooltip(true)}
@@ -67,7 +66,6 @@ export default function Navbar() {
                 Sign In Attendance
               </span>
             </>
-          ) : null}
           {status === "authenticated" ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
